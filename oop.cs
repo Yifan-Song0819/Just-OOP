6380dee3f9d11c2c068ecd638245fbb36eef2847
Yifan1989 <j.codes1989@gmail.com>
2019-07-04 10:20:07 +1200

    1

--- oop.cs
+++ oop.cs
@@ -3,11 +3,28 @@ using System;
 public class OOP{
     public static void Main(String[] args){
         A a = new A();
-
-        Console.WriteLine(a.value);
+        // Console.WriteLine(a.value);
         Console.WriteLine("Please input");
         string user_input = Console.ReadLine();
-        Console.WriteLine(user_input);
+        int guess = 0;
+        Int32.TryParse(user_input, out guess);
+        //Console.WriteLine(guess);
+        while (guess != a.value){
+            if (guess > a.value){
+                Console.WriteLine("Input is larger than correct number!");
+            }
+            else{
+                Console.WriteLine("Input is smaller than correct number!");
+            }
+            Console.WriteLine("Please input");
+            user_input = Console.ReadLine();
+            guess = 0;
+            Int32.TryParse(user_input, out guess);
+        }
+        if (guess == a.value){
+            Console.WriteLine("bingo!");
+        }
+
     }
 }
 

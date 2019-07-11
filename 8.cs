8c8fd6af313cfd6f500a18d04e660e7006d64dff
Yifan1989 <j.codes1989@gmail.com>
2019-07-11 23:50:06 +1200

    8

--- 8.cs
+++ 8.cs
@@ -21,32 +21,41 @@ public class Vehicles{
 public class Car : Vehicles{
     public int seats;
 
-    public Car(string brand, string color) : base(brand, color){
+    public Car(string brand, string color, int seats) : base(brand, color){
         this.brand = brand;
         this.color = color;
+        this.seats = seats;
     }
 
     public void showCar(){
-        Console.WriteLine("this car has a " + this.color + " car and its brand is " + this.brand +".");
+        Console.WriteLine("this car has a " + this.color + " color and its brand is " + this.brand +" and " + this.seats + " seats.");
     }
 }
 
 public class Truck : Vehicles{
-    public float seats;
+    public float load;
 
-    public Truck(string brand, string color) : base(brand, color){
+    public Truck(string brand, string color, float load) : base(brand, color){
         this.brand = brand;
         this.color = color;
+        this.load = load;
     }
 
+
     public void showTruck(){
-        Console.WriteLine("this truck has a " + this.color + " car and its brand is " + this.brand +".");
+        Console.WriteLine("this truck has a " + this.color + " color and its brand is " + this.brand +"." + this.load);
+        // Console.WriteLine(this.brand);
     }
 }
 
 public class Test{
     public static void Main(String[] args){
-        Car a_car = new Car("toyota", "red");
+        Car a_car = new Car("toyota", "red", 5);
         a_car.showCar();
+
+
+        Truck a_truck = new Truck("ranger", "blue", 18);
+        a_truck.showTruck();
+
     }
 }
